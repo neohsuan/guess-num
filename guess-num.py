6a3425ae32f585e1e392b87a02c64c784a49e8ed
Neo Lee <hsuan741220@gmail.com>
2020-08-19 23:35:22 +0800

    final version

--- guess-num.py
+++ guess-num.py
@@ -1,13 +1,17 @@
 import random
+start = input('請決定隨機數字開始值： ')
+end = input("請決定隨機數字結束值： ")
+start = int(start)
+end = int(end)
 
-ans = random.randint(1, 100)
+ans = random.randint(start, end)
 count = 0
 while True:
     count += 1 # count = count + 1
     num = input('請猜一個數字： ')
     num = int(num)
     if num == ans:
-        print('終於猜對了')
+        print('終於猜對了!!!')
         print('這是你猜的第', count, '次')
         break
     elif num < ans:
